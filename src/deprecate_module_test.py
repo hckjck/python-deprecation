678bebb0d802c01bedc8b1062a304879e914f5db
Michael Handerek <michael.handerek@medneo.com>
2020-02-07 19:41:10 +0100

    Fix id support note

--- src/deprecate_module_test.py
+++ src/deprecate_module_test.py
@@ -7,7 +7,7 @@ Supported IDE's:
     Name                Version     Supported
     -----------------------------------------
     Atom                3.40.0      No
-    PyCharm             2019.3      No
+    PyCharm             2019.3      Yes
     Sublime Text        3.2.2       No
 
 """
